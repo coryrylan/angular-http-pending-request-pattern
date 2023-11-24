7b18b8090465fbdd51663219506cbdebd8c5fbda
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-23 20:36:19 -0600

    4 files modified, 4 added, 7 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { UserService, User, Pending, Status } from './user.service';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  template: `
+    <a href="https://coryrylan.com/blog/angular-http-pending-request-pattern">
+      <h1>Tutorial: Angular HTTP Pending Request Pattern</h1>
+    </a>
 
-import { AppModule } from './app/app.module';
+    <section *ngIf="user.data | async as user">
+      <h3>{{user.name}}</h3>
+      <p>Height: {{user.height}}</p>
+      <p>Mass: {{user.mass}}</p>
+      <p>Homeworld: {{user.homeworld}}</p>
+    </section>
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+    <section [ngSwitch]="user.status | async">
+      <span *ngSwitchCase="Status.LOADING">Loading User...</span>
+      <span *ngSwitchCase="Status.ERROR">There was an error loading the user.</span>
+    </section>
+  `,
+})
+export class App {
+  readonly Status = Status;
+  readonly user: Pending<User>;
+
+  constructor(private userService: UserService) {
+    this.user = this.userService.load(1);
   }
-  window['ngRef'] = ref;
+}
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+bootstrapApplication(App);
